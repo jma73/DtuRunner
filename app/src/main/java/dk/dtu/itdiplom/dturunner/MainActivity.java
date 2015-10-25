d4e9f5989115d98415196ae09e3519303d4aaf1b
jma73 <jma73@hotmail.com>
2015-10-25 19:00:47 +0100

    import af google api location...

--- app/src/main/java/dk/dtu/itdiplom/dturunner/MainActivity.java
+++ app/src/main/java/dk/dtu/itdiplom/dturunner/MainActivity.java
@@ -1,12 +1,18 @@
 package dk.dtu.itdiplom.dturunner;
 
+//import android.location.LocationListener;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// Disse imports er til for at kunne anvende fused location:
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+
 public class MainActivity extends AppCompatActivity
-
-
-
+        implements
+            GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener
 {
 
     @Override
@@ -16,6 +22,23 @@ public class MainActivity extends AppCompatActivity
     }
 
 
+    @Override
+    public void onLocationChanged(Location location) {
 
+    }
 
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
 }
