6debd5ba7e12aef27f551f83db1cfc0d4ecf8a1a
jan <jma73@hotmail.com>
2015-11-05 16:31:39 +0100

    småting

--- app/src/main/java/dk/dtu/itdiplom/dturunner/FragmentLoeb.java
+++ app/src/main/java/dk/dtu/itdiplom/dturunner/FragmentLoeb.java
@@ -24,7 +24,11 @@ public class FragmentLoeb extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_loeb, container, false);
+        View rod = inflater.inflate(R.layout.fragment_loeb, container, false);
+
+
+
+        return rod;
     }
 
 
