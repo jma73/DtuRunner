6e7fc84fb7027a94778a65e105c4c034cf4ad123
jan <jma73@hotmail.com>
2015-11-05 17:25:10 +0100

    så virker Fused location. pt. logges der kun. så kig i liggen...

--- app/src/main/java/dk/dtu/itdiplom/dturunner/FragmentLoeb.java
+++ app/src/main/java/dk/dtu/itdiplom/dturunner/FragmentLoeb.java
@@ -1,19 +1,80 @@
 package dk.dtu.itdiplom.dturunner;
 
 
+import android.location.Location;
 import android.os.Bundle;
 //import android.app.Fragment;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+// Disse imports er til for at kunne anvende fused location:
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 /**
- * A simple {@link Fragment} subclass.
+ * Dette fragment skal indeholde selve løbsaktiviteten
  */
-public class FragmentLoeb extends Fragment {
+public class FragmentLoeb extends Fragment implements
+        View.OnClickListener,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener
 
+{
+
+    protected static final String TAG = "JJ-location-updates";
+
+    /**
+     * The desired interval for location updates. Inexact. Updates may be more or less frequent.
+     */
+    public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+
+    /**
+     * The fastest rate for active location updates. Exact. Updates will never be more frequent
+     * than this value.
+     */
+    public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
+            UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+
+    /**
+     * Provides the entry point to Google Play services.
+     */
+    protected GoogleApiClient mGoogleApiClient;
+
+
+    /**
+     * Stores parameters for requests to the FusedLocationProviderApi.
+     */
+    protected LocationRequest mLocationRequest;
+
+    /**
+     * Time when the location was updated represented as a String.
+     */
+    protected String mLastUpdateTime;
+
+    /**
+     * Represents a geographical location.
+     */
+    protected Location mCurrentLocation;
+
+    // Labels.
+    protected String mLatitudeLabel;
+    protected String mLongitudeLabel;
+    protected String mLastUpdateTimeLabel;
+
+    protected Boolean mRequestingLocationUpdates;
+
+
+
+    private Button buttonStartAktivitet;
 
     public FragmentLoeb() {
         // Required empty public constructor
@@ -27,9 +88,189 @@ public class FragmentLoeb extends Fragment {
         View rod = inflater.inflate(R.layout.fragment_loeb, container, false);
 
 
+        buttonStartAktivitet = (Button) rod.findViewById(R.id.buttonStartAktivitet);
+        buttonStartAktivitet.setOnClickListener(this);
+
+        // Set labels.
+        mLatitudeLabel = ("latitude_label");
+        mLongitudeLabel = ("longitude_label");
+        mLastUpdateTimeLabel = "last_update_time_label";
+
+        mRequestingLocationUpdates = false;
+        mLastUpdateTime = "";
+
+
+        // Kick off the process of building a GoogleApiClient and requesting the LocationServices
+        // API.
+        buildGoogleApiClient();
 
         return rod;
     }
 
 
+    //region overførte metoder
+    /**
+     * Builds a GoogleApiClient. Uses the {@code #addApi} method to request the
+     * LocationServices API.
+     *
+     */
+    protected synchronized void buildGoogleApiClient() {
+        Log.i(TAG, "Building GoogleApiClient");
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())    // todo jan - måtte ændre fra this til getContext() eller getActivity(). brug getActivity() siger Jakob
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)               // todo jan - her vælges Location...
+                .build();
+        createLocationRequest();
+    }
+
+    /**
+     * Sets up the location request. Android has two location request settings:
+     * {@code ACCESS_COARSE_LOCATION} and {@code ACCESS_FINE_LOCATION}. These settings control
+     * the accuracy of the current location. This sample uses ACCESS_FINE_LOCATION, as defined in
+     * the AndroidManifest.xml.
+     * <p/>
+     * When the ACCESS_FINE_LOCATION setting is specified, combined with a fast update
+     * interval (5 seconds), the Fused Location Provider API returns location updates that are
+     * accurate to within a few feet.
+     * <p/>
+     * These settings are appropriate for mapping applications that show real-time location
+     * updates.
+     */
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+
+        // Sets the desired interval for active location updates. This interval is
+        // inexact. You may not receive updates at all if no location sources are available, or
+        // you may receive them slower than requested. You may also receive updates faster than
+        // requested if other applications are requesting location at a faster interval.
+        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        // Sets the fastest rate for active location updates. This interval is exact, and your
+        // application will never receive updates faster than this value.
+        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    //endregion
+
+
+
+    @Override
+    public void onClick(View v) {
+        if(v== buttonStartAktivitet)
+        {
+            // 1) tjek for location enabled
+            startLoebsAktivitetOgLocationUpdates();
+        }
+    }
+
+    // todo jan old name: startUpdatesButtonHandler. rename til noget mere sigende!!!
+    //      public void startUpdatesButtonHandler(View view)
+    public void startLoebsAktivitetOgLocationUpdates() {
+        if (!mRequestingLocationUpdates)
+        {
+            mRequestingLocationUpdates = true;
+            //      setButtonsEnabledState();   // todo jan - evt. med senere eller fjern.
+            startLocationUpdates();
+        }
+    }
+
+    /**
+     * Requests location updates from the FusedLocationApi.
+     */
+    protected void startLocationUpdates() {
+
+        // tjek for googleAPI:
+
+
+        // The final argument to {@code requestLocationUpdates()} is a LocationListener
+        // (http://developer.android.com/reference/com/google/android/gms/location/LocationListener.html).
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    /**
+     * Removes location updates from the FusedLocationApi.
+     */
+    protected void stopLocationUpdates() {
+        // It is a good practice to remove location requests when the activity is in a paused or
+        // stopped state. Doing so helps battery performance and is especially
+        // recommended in applications that request frequent location updates.
+
+        // The final argument to {@code requestLocationUpdates()} is a LocationListener
+        // (http://developer.android.com/reference/com/google/android/gms/location/LocationListener.html).
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d(TAG, "onLocationChanged " + location.getLatitude());
+        // todo jan
+    }
+
+    //region Connection Callbacks
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG, " * * * Connected to GoogleApiClient * * * ");
+
+        // If the initial location was never previously requested, we use
+        // FusedLocationApi.getLastLocation() to get it. If it was previously requested, we store
+        // its value in the Bundle and check for it in onCreate(). We
+        // do not request it again unless the user specifically requests location updates by pressing
+        // the Start Updates button.
+        //
+        // Because we cache the value of the initial location in the Bundle, it means that if the
+        // user launches the activity,
+        // moves to a new location, and then changes the device orientation, the original location
+        // is displayed as the activity is re-created.
+        if (mCurrentLocation == null) {
+            mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+            updateUI();
+        }
+
+        // If the user presses the Start Updates button before GoogleApiClient connects, we set
+        // mRequestingLocationUpdates to true (see startUpdatesButtonHandler()). Here, we check
+        // the value of mRequestingLocationUpdates and if it is true, we start location updates.
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    /**
+     * Updates the latitude, the longitude, and the last location time in the UI.
+     */
+    private void updateUI() {
+
+        Log.d(TAG, String.format("%s: %f", mLatitudeLabel,
+                mCurrentLocation.getLatitude()));
+
+
+//        mLatitudeTextView.setText(String.format("%s: %f", mLatitudeLabel,
+//                mCurrentLocation.getLatitude()));
+//        mLongitudeTextView.setText(String.format("%s: %f", mLongitudeLabel,
+//                mCurrentLocation.getLongitude()));
+//        mLastUpdateTimeTextView.setText(String.format("%s: %s", mLastUpdateTimeLabel,
+//                mLastUpdateTime));
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+    //endregion Connection Callbacks
 }
