f5f3c7f9ccdb2ef28e94de91ce51b196aa98fecf
jan <jma73@hotmail.com>
2015-12-03 18:26:58 +0100

    udkommenteret ubrugte klasser

--- app/src/main/java/dk/dtu/itdiplom/dturunner/Slettes/LocationInfo.java
+++ app/src/main/java/dk/dtu/itdiplom/dturunner/Slettes/LocationInfo.java
@@ -1,40 +1,40 @@
-package dk.dtu.itdiplom.dturunner.Slettes;
-
-import java.util.Date;
-
-/**
- * Created by JanMøller on 31-10-2015.
- */
-public class LocationInfo {
-
-    private Date mTimestamp;
-    private double mLatitude;
-    private double mLongitude;
-
-    // distance? speed? heart
-
-    // methods todo jan:
-    //   - get printable / tostring
-    //   -
-
-    public void LocationInfo() {
-    }
-
-    public double getLatitude(){
-        return mLatitude;
-    }
-
-    public void setLatitude(double latitude){
-        mLatitude = latitude;
-    }
-
-    public double getLongitude(){
-        return mLongitude;
-    }
-
-    public void setLongitude(double longitude){
-        mLongitude = longitude;
-    }
-
-
-}
+//package dk.dtu.itdiplom.dturunner.Slettes;
+//
+//import java.util.Date;
+//
+///**
+// * Created by JanMøller on 31-10-2015.
+// */
+//public class LocationInfo {
+//
+//    private Date mTimestamp;
+//    private double mLatitude;
+//    private double mLongitude;
+//
+//    // distance? speed? heart
+//
+//    // methods todo jan:
+//    //   - get printable / tostring
+//    //   -
+//
+//    public void LocationInfo() {
+//    }
+//
+//    public double getLatitude(){
+//        return mLatitude;
+//    }
+//
+//    public void setLatitude(double latitude){
+//        mLatitude = latitude;
+//    }
+//
+//    public double getLongitude(){
+//        return mLongitude;
+//    }
+//
+//    public void setLongitude(double longitude){
+//        mLongitude = longitude;
+//    }
+//
+//
+//}
